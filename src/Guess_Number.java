90903e0444a0dc6afd005d1f50e92b18ef5c7c51
peferso <p.fdez.s.90@gmail.com>
2021-11-14 21:21:31 +0100

    dia 1 de clases

--- src/Guess_Number.java
+++ src/Guess_Number.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Guess_Number {
 
 	/**
@@ -6,7 +6,40 @@ public class Guess_Number {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int rnum = (int)(Math.random()*100);
+		
+		//System.out.println(rnum);
+		
+		Scanner entrada=new Scanner(System.in);
+		
+		int numero=0;
+		
+		int intentos=0;
+		
+		while(numero!=rnum){
+			
+			intentos++;
+			
+			System.out.println("Enter a number, please");
+			
+			numero=entrada.nextInt();
+			
+			if(rnum<numero){
+				
+				System.out.println("You entered a number larger than the secret one.");
+				
+			}
+			
+			else if(rnum>numero){
+				
+				System.out.println("You entered a number smaller than the secret one.");
+				
+			}
+			
+		}
+		System.out.println("Correct! (" + intentos + " attempts in total)");
+		
 	}
 
 }
